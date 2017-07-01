9d092d64709abf6dc26c2a6364d95cda5ddb08d4
mutexd <joshd1986@gmail.com>
2017-07-01 19:59:21 +0800

    [full-stack] parameterize  for Token
    Signed-off-by: mutexd <joshd1986@gmail.com>

--- app/user-page/recommend.js
+++ app/user-page/recommend.js
@@ -3,33 +3,69 @@
 angular.
   module('movieApp').
     factory('recommendSvc', ['$resource', function($resource){
-	  return $resource('http://localhost:5000/webmovie/api/v0.1/recommend/:userId', {userId:'@userId'});
+      return {
+		api: function(token){
+		  return $resource('http://localhost:5000/webmovie/api/v0.1/recommend/:userId?begin=:begin&end=:end',
+			{userId:'@userId', begin:'@begin', end:'@end'},{
+			  query: {
+				method: 'GET',
+				headers: {
+				  'Authorization': 'Bearer ' + token
+				}
+			  }
+			});
+		}
+      }
 	}]);
 
 // service for rated movies
 angular.
   module('movieApp').
     factory('ratedSvc', ['$resource', function($resource){
-	  return $resource('http://localhost:5000/webmovie/api/v0.1/rated/:userId', {userId:'@userId'});
+      return {
+		api: function(token){
+		  return $resource('http://localhost:5000/webmovie/api/v0.1/rated/:userId?begin=:begin&end=:end',
+			{userId:'@userId', begin:'@begin', end:'@end'},{
+			  query: {
+				method: 'GET',
+				headers: {
+				  'Authorization': 'Bearer ' + token
+				}
+			  }
+			});
+		}
+      }
 	}]);
-
-// testing 
-angular.
-  module('movieApp').
-    factory('rankedSvc', ['$resource', function($resource){
-      return $resource('http://localhost:5000/webmovie/api/v0.1/guest?begin=0&end=20', {});
-    }]);
 
 // controller for user page
 angular.
   module('movieApp').
-    controller('userController', ['recommendSvc', 'ratedSvc', 'rankedSvc', 'userService',
-         function(recommendSvc, ratedSvc, rankedSvc, userService){
-      var self = this
-      var fields = {userId:userService.getUID()}
-      console.error(userService.getToken())
-      self.recommendRequest = recommendSvc.save(fields)
-      self.ratedRequest = ratedSvc.save(fields)
-      self.movieList = rankedSvc.query()
+    controller('userController', ['recommendSvc', 'ratedSvc', 'userService',
+         function(recommendSvc, ratedSvc, userService){
+      var self = this;
+      var fields = {userId:userService.getUID(), begin:0, end:20};
+      var token = userService.getToken();
+      recommendSvc.api(token).query(fields,
+		function(data){
+		  if(data.status == "fail"){
+			console.error(data);
+		  } else {
+			self.movieList = data;
+		  }
+		}, function(error){
+		  console.error(error);
+		}
+	  );
+      ratedSvc.api(token).query(fields,
+		function(data){
+		  if(data.status == "fail"){
+			console.error(data);
+		  } else {
+			console.error(data);
+		  }
+		}, function(error){
+		  console.error(error);
+		}
+	  );
 	}]);
 
